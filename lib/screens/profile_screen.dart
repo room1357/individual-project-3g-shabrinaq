65d5d892df25aa2b84f55680ca08abdf8779da7e
shabrinaq <145519077+shabrinaq@users.noreply.github.com>
2025-10-29 14:18:08 +0700

    P2 : Multi-User Expense App_User Profiles

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,7 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert'; 
+import '../services/auth_service.dart';
+import 'login_screen.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  String username = '';
+  String email = '';
+  String fullName = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserProfile();
+  }
+
+  Future<void> _loadUserProfile() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userJson = prefs.getString('current_user');
+
+    if (userJson != null) {
+      final userData = jsonDecode(userJson); 
+      setState(() {
+        username = userData['username'] ?? '';
+        email = userData['email'] ?? '';
+        fullName = userData['fullName'] ?? '';
+      });
+    }
+  }
+
+  // Logout user
+  Future<void> _logout(BuildContext context) async {
+    await AuthService.logout();
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Logout berhasil!')),
+    );
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => const LoginScreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +72,12 @@ class ProfileScreen extends StatelessWidget {
             ElevatedButton(
               onPressed: () => Navigator.pop(context),
               child: Text('Back'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () => _logout(context), // digunakan untuk memanggil fungsi logout
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              child: const Text('Logout'),
             ),
           ],
         ),
